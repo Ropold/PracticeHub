30cc7178b397713c34226695a55fe42a88d55483
Robert Stolz <robert.stolz87@gmail.com>
2024-11-29 09:19:10 +0100

    wrong enpoint in UserintegrationTest & WithMockUser was missing in RoomControllerIntegrationTest after security implementation

--- backend/src/main/java/ropold/backend/security/UserController.java
+++ backend/src/main/java/ropold/backend/security/UserController.java
@@ -1,14 +1,9 @@
 package ropold.backend.security;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.core.context.SecurityContextHolder;
-import org.springframework.security.oauth2.core.user.OAuth2User;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.security.Security;
 
 @RestController
 @RequestMapping("/api/users")
@@ -20,11 +15,4 @@ public class UserController {
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
 
-//    @GetMapping("/me")
-//    public String getMe(@AuthenticationPrincipal OAuth2User user) {
-//        if (user == null) {
-//            return "anonymousUser";
-//        }
-//        return user.getAttributes().get("login").toString(); //github username
-//    }
 }
