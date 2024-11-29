ea99b0deadd49d6ba9c0faafd108752922c87152
Robert Stolz <robert.stolz87@gmail.com>
2024-11-29 14:15:56 +0100

    eHandler test - MethodArgumentNotValidException

--- backend/src/test/java/ropold/backend/exception/ExceptionHandlerTest.java
+++ backend/src/test/java/ropold/backend/exception/ExceptionHandlerTest.java
@@ -4,8 +4,11 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
@@ -23,5 +26,34 @@ class ExceptionHandlerTest {
                 .andExpect(status().isNotFound())
                 .andExpect(jsonPath("$.message").value("No Room found with id: non-existing-id"));
     }
+
+    @Test
+    @WithMockUser(username = "testUser", roles = {"USER"})
+    void postRoom_shouldFailValidation_whenAllFieldsExceptWishlistStatusAreInvalid() throws Exception {
+        // WHEN
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/practice-hub")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content("""
+                     {
+                        "name": "",
+                        "address": "Invalid Address",
+                        "category": "",
+                        "description": "",
+                        "wishlistStatus": "ON_WISHLIST"
+                     }
+                     """)
+                )
+                // THEN
+                .andExpect(status().isBadRequest())
+                .andExpect(MockMvcResultMatchers.content().json("""
+        {
+            "name": "size must be between 3 and 2147483647",
+            "address": "Address must contain at least a street name, a 5-digit postal code, and a city name, e.g. 'Musterstraße 12345 Musterstadt'",
+            "category": "must not be blank",
+            "description": "must not be blank"
+        }
+    """));
+    }
+
 
 }
