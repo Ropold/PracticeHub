dada0f28a8b749915aa4148b137468833bce0225
Robert Stolz <robert.stolz87@gmail.com>
2024-11-29 10:02:13 +0100

    in usercontroller changed @GetMapping(value = "/me", produces = "text/plain")

--- backend/src/main/java/ropold/backend/security/UserController.java
+++ backend/src/main/java/ropold/backend/security/UserController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 public class UserController {
 
-    @GetMapping("/me")
+    @GetMapping(value = "/me", produces = "text/plain")
     public String getMe() {
         return SecurityContextHolder.getContext().getAuthentication().getName();
     }
