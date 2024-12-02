8edc14d9eb98a90b5f405323961baf6070a914ad
Robert Stolz <robert.stolz87@gmail.com>
2024-12-02 13:04:18 +0100

    change validation

--- backend/src/main/java/ropold/backend/model/RoomModelDto.java
+++ backend/src/main/java/ropold/backend/model/RoomModelDto.java
@@ -1,7 +1,6 @@
 package ropold.backend.model;
 
 import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
 
 public record RoomModelDto(
@@ -10,10 +9,7 @@ public record RoomModelDto(
         String name,
 
         @NotBlank
-        @Pattern(
-                regexp = "^.*?(\\b[A-Za-zäöüßÄÖÜ]+(?:straße|platz|weg|gasse|allee|ring|bahn|tor|damm|ufer)?\\b).*?\\b\\d{5}\\b.*?[A-Za-zäöüßÄÖÜ]+.*$",
-                message = "Address must contain at least a street name, a 5-digit postal code, and a city name, e.g. 'Musterstraße 12345 Musterstadt'"
-        )
+        @Size(min = 11, message = "Address must contain at least two words, a 5-digit postal code, and a city name, e.g. 'Musterstraße 12345 Musterstadt'")
         String address,
 
         @NotBlank
