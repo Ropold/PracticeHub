c1ae9826960b3c4a330d1e9607ab67365d850d2b
Robert Stolz <robert.stolz87@gmail.com>
2024-12-02 18:25:53 +0100

    ehandler works

--- backend/src/test/java/ropold/backend/exception/ExceptionHandlerTest.java
+++ backend/src/test/java/ropold/backend/exception/ExceptionHandlerTest.java
@@ -1,15 +1,26 @@
 package ropold.backend.exception;
 
+import com.cloudinary.Cloudinary;
+import com.cloudinary.Uploader;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import ropold.backend.repository.RoomRepository;
 
+import java.util.Map;
+
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyMap;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @SpringBootTest
@@ -18,6 +29,12 @@ class ExceptionHandlerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @Autowired
+    RoomRepository roomRepository;
+
+    @MockBean
+    private Cloudinary cloudinary;
 
     @Test
     void whenRoomNotFoundException_thenReturnsNotFound() throws Exception {
@@ -30,24 +47,26 @@ class ExceptionHandlerTest {
     @WithMockUser(username = "testUser", roles = {"USER"})
     void postRoom_shouldFailValidation_whenAllFieldsExceptWishlistStatusAreInvalid() throws Exception {
         // WHEN
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/practice-hub")
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content("""
-                {
-                   "name": "hu",
-                   "address": "Neumarkt Köln",
-                   "category": "   ",
-                   "description": "huhu",
-                   "wishlistStatus": "ON_WISHLIST"
-                }
-                """)
-                )
+        roomRepository.deleteAll();
+        Uploader mockuploader = mock(Uploader.class);
+        when(mockuploader.upload(any(), anyMap())).thenReturn(Map.of("secure_url", "https://www.test.de/"));
+        when(cloudinary.uploader()).thenReturn(mockuploader);
+
+        mockMvc.perform(MockMvcRequestBuilders.multipart("/api/practice-hub")
+                        .file(new MockMultipartFile("image", "test.jpg", "image/jpeg", "test image".getBytes()))
+                        .file(new MockMultipartFile("roomModelDto", "","application/json", """
+                        {
+                            "name": "G",
+                            "address": "Neumarkt",
+                            "category": "   ",
+                            "description": "Ein traditionsreicher Saal für Konzerte und Veranstaltungen.",
+                            "wishlistStatus": "ON_WISHLIST"
+                        }
+                        """.getBytes())))
                 // THEN
                 .andExpect(status().isBadRequest())
                 .andExpect(MockMvcResultMatchers.content().json("""
-                 {"address":"Address must contain at least a street name, a 5-digit postal code, and a city name, e.g. 'Musterstraße 12345 Musterstadt'",
-                 "name":"size must be between 3 and 2147483647",
-                 "category":"must not be blank"}
+                {"address":"Address must contain at least two words, a 5-digit postal code, and a city name, e.g. 'Musterstraße 12345 Musterstadt'","name":"Name must contain at least 3 characters","category":"must not be blank"}
                 """));
     }
 }
