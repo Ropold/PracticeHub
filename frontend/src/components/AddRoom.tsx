7b4eaf042159edfdaac152f8045c86e7c2176470
Robert Stolz <robert.stolz87@gmail.com>
2024-12-05 17:05:22 +0100

    small changes

--- frontend/src/components/AddRoom.tsx
+++ frontend/src/components/AddRoom.tsx
@@ -10,8 +10,6 @@ type AddRoomProps = {
 
 export default function AddRoom(props: Readonly<AddRoomProps>) {
 
-    console.log("User from props:", typeof props.user, props.user);
-
     const [name, setName] = useState<string>("");
     const [address, setAddress] = useState<string>("");
     const [category, setCategory] = useState<string>("");
@@ -30,11 +28,9 @@ export default function AddRoom(props: Readonly<AddRoomProps>) {
             data.append("image", image);
         }
 
-        const roomData = { name, address, category, description, appUserGithubId:props.user, imageUrl: "" };
+        const roomData = {name, address, category, description, appUserGithubId: props.user, imageUrl: ""};
 
-        console.log("roomData:", roomData);
-
-        data.append("roomModelDto", new Blob([JSON.stringify(roomData)], { type: "application/json" }));
+        data.append("roomModelDto", new Blob([JSON.stringify(roomData)], {type: "application/json"}));
 
         axios
             .post("/api/practice-hub", data, {
@@ -65,7 +61,7 @@ export default function AddRoom(props: Readonly<AddRoomProps>) {
     };
 
     const onFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-        if (e.target.files){
+        if (e.target.files) {
             setImage(e.target.files[0]);
         }
     }
@@ -75,22 +71,26 @@ export default function AddRoom(props: Readonly<AddRoomProps>) {
             <div className="edit-form">
                 <h2>Add New Room</h2>
                 <form onSubmit={handleSubmit}>
-                    <label>Name: <input className="input-small" type="text" value={name}
-                            onChange={(e) => setName(e.target.value)}/></label>
+                    <label>Title: <input className="input-small" type="text" value={name}
+                                        onChange={(e) => setName(e.target.value)}/></label>
                     <label>Address: <input className="input-small" type="text" value={address}
-                            onChange={(e) => setAddress(e.target.value)}/></label>
+                                           onChange={(e) => setAddress(e.target.value)}/></label>
                     <label>Category: <input className="input-small" type="text" value={category}
-                            onChange={(e) => setCategory(e.target.value)}/></label>
+                                            onChange={(e) => setCategory(e.target.value)}/></label>
                     <label>Description: <textarea className="textarea-large" value={description}
-                            onChange={(e) => setDescription(e.target.value)}/></label>
-                    <label>Status: <select>
-                            <option>On Wishlist</option>
-                            <option>Not on Wishlist</option></select></label>
+                                                  onChange={(e) => setDescription(e.target.value)}/></label>
                     <input type={"file"} onChange={onFileChange}/>
                     {image && <img src={URL.createObjectURL(image)} className={"room-card-image"}/>}
-                    <div className="button-group"><button type="submit">Add Room</button></div>
+                    <div className="button-group">
+                        <button type="submit">Add Room</button>
+                    </div>
                 </form>
             </div>
         </div>
     );
 }
+
+// <label>Status: <select>
+//     <option>On Wishlist</option>
+//     <option>Not on Wishlist</option>
+// </select></label>
