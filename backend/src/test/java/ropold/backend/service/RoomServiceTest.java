8cfa5cbc1d115030c14553113822e0c5be549635
Robert Stolz <robert.stolz87@gmail.com>
2024-12-06 16:56:46 +0100

    room service tests works again

--- backend/src/test/java/ropold/backend/service/RoomServiceTest.java
+++ backend/src/test/java/ropold/backend/service/RoomServiceTest.java
@@ -1,7 +1,6 @@
 package ropold.backend.service;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
 import ropold.backend.model.Category;
 import ropold.backend.model.RoomModel;
 import ropold.backend.repository.RoomRepository;
@@ -38,90 +37,87 @@ class RoomServiceTest {
         // Then
         assertEquals(expected, rooms);
     }
-//
-//    @Test
-//    void getRoomById() {
-//        // Given
-//        when(roomRepository.findById("1")).thenReturn(Optional.of(roomModel));
-//
-//        // When
-//        RoomModel expected = testRoomService.getRoomById("1");
-//
-//        // Then
-//        assertEquals(expected, roomModel);
-//    }
-//
-//    @Test
-//    void addRoom() {
-//        // Given
-//        RoomModel roomModel3 = new RoomModel("3", "Gürzenich Saal", "Neumarkt 1, 50667 Köln", "Orchester-Saal", "Ein traditionsreicher Saal für Konzerte und Veranstaltungen.", WishlistStatus.NOT_ON_WISHLIST, "https://www.test.de/");
-//        RoomModel newRoom = new RoomModel("3", roomModel3.name(), roomModel3.address(), roomModel3.category(), roomModel3.description(), roomModel3.wishlistStatus(), roomModel3.imageUrl());
-//
-//        when(idService.generateRandomId()).thenReturn("3");
-//        when(roomRepository.save(any(RoomModel.class))).thenReturn(newRoom);
-//
-//        // When
-//        RoomModel expected = testRoomService.addRoom(roomModel3);
-//
-//        // Then
-//        assertEquals(newRoom, expected);
-//        assertEquals(roomModel3.name(), expected.name());
-//        assertEquals(roomModel3.address(), expected.address());
-//        assertEquals(roomModel3.category(), expected.category());
-//        assertEquals(roomModel3.description(), expected.description());
-//        assertEquals(roomModel3.wishlistStatus(), expected.wishlistStatus());
-//    }
-//
-//    @Test
-//    void updateRoomWithPut() {
-//        // Given
-//        RoomModel existingRoom = new RoomModel(
-//                "1",
-//                "Gürzenich Saal",
-//                "Neumarkt 1, 50667 Köln",
-//                "Orchester-Saal",
-//                "Ein traditionsreicher Saal für Konzerte und Veranstaltungen.",
-//                WishlistStatus.NOT_ON_WISHLIST,
-//                "https://www.test.de/"
-//        );
-//
-//        RoomModel updatedRoom = new RoomModel(
-//                "1",
-//                existingRoom.name(),
-//                existingRoom.address(),
-//                existingRoom.category(),
-//                existingRoom.description(),
-//                WishlistStatus.ON_WISHLIST,
-//                "https://www.test.de/"
-//        );
-//
-//        when(roomRepository.existsById("1")).thenReturn(true);
-//        when(roomRepository.save(any(RoomModel.class))).thenReturn(updatedRoom);
-//
-//        // When
-//        RoomModel expected = testRoomService.updateRoomWithPut("1", updatedRoom);
-//
-//        // Then
-//        assertEquals(updatedRoom, expected);
-//        verify(roomRepository, times(1)).existsById("1");
-//        verify(roomRepository, times(1)).save(updatedRoom);
-//    }
-//
-//    @Test
-//    void deleteRoomTest() {
-//        String fixedId = "123e4567-e89b-12d3-a456-426614174000";
-//        RoomRepository roomRepositoryMock = mock(RoomRepository.class);
-//        CloudinaryService cloudinaryServiceMock = mock(CloudinaryService.class);
-//
-//        RoomModel room = new RoomModel(fixedId, "Test Room", "Test Address", "Test Category", "Test Description", WishlistStatus.ON_WISHLIST, null);
-//        when(roomRepositoryMock.findById(fixedId)).thenReturn(Optional.of(room));
-//
-//        RoomService roomService = new RoomService(new IdService(), roomRepositoryMock, cloudinaryServiceMock);
-//
-//        roomService.deleteRoom(fixedId);
-//
-//        verify(roomRepositoryMock).deleteById(fixedId);
-//        verify(roomRepositoryMock).findById(fixedId);
-//    }
+    @Test
+    void getRoomById() {
+        // Given
+        when(roomRepository.findById("1")).thenReturn(Optional.of(roomModel));
 
+        // When
+        RoomModel expected = testRoomService.getRoomById("1");
+
+        // Then
+        assertEquals(expected, roomModel);
+    }
+
+    @Test
+    void addRoom() {
+        // Given
+        RoomModel roomModel3 = new RoomModel(null, "Gürzenich Saal", "Neumarkt 1, 50667 Köln", Category.ORCHESTER_HALL, "Ein traditionsreicher Saal für Konzerte und Veranstaltungen.",
+                "154427648", "Ropold", "https://avatars.githubusercontent.com/u/154427648?v=4", "https://github.com/Ropold", true, null);
+        RoomModel newRoom = new RoomModel("3", roomModel3.name(), roomModel3.address(), roomModel3.category(), roomModel3.description(),
+                roomModel3.appUserGithubId(), roomModel3.appUserUsername(), roomModel3.appUserAvatarUrl(), roomModel3.appUserGithubUrl(),
+                roomModel3.isActive(), "https://www.test.de/");
+
+        when(idService.generateRandomId()).thenReturn("3");
+        when(roomRepository.save(any(RoomModel.class))).thenReturn(newRoom);
+
+        // When
+        RoomModel expected = testRoomService.addRoom(roomModel3);
+
+        // Then
+        assertEquals(newRoom, expected);
+    }
+
+    @Test
+    void updateRoomWithPut() {
+        // Given
+        RoomModel existingRoom = new RoomModel(
+                "1",
+                "Gürzenich Saal",
+                "Neumarkt 1, 50667 Köln",
+                Category.ORCHESTER_HALL,
+                "Ein traditionsreicher Saal für Konzerte und Veranstaltungen.",
+                "154427648", "Ropold", "https://avatars.githubusercontent.com/u/154427648?v=4",
+                "https://github.com/Ropold", true, "https://www.test.de/"
+        );
+
+        RoomModel updatedRoom = new RoomModel(
+                "1",
+                existingRoom.name(),
+                existingRoom.address(),
+                existingRoom.category(),
+                existingRoom.description(),
+                existingRoom.appUserGithubId(),
+                existingRoom.appUserUsername(),
+                existingRoom.appUserAvatarUrl(),
+                existingRoom.appUserGithubUrl(),
+                false, // Updated isActive value
+                "https://www.test.de/"
+        );
+
+        when(roomRepository.existsById("1")).thenReturn(true);
+        when(roomRepository.save(any(RoomModel.class))).thenReturn(updatedRoom);
+
+        // When
+        RoomModel expected = testRoomService.updateRoomWithPut("1", updatedRoom);
+
+        // Then
+        assertEquals(updatedRoom, expected);
+        verify(roomRepository, times(1)).existsById("1");
+        verify(roomRepository, times(1)).save(updatedRoom);
+    }
+
+    @Test
+    void deleteRoomTest() {
+        // Given
+        String fixedId = "1";
+        when(roomRepository.findById(fixedId)).thenReturn(Optional.of(roomModel));
+
+        // When
+        testRoomService.deleteRoom(fixedId);
+
+        // Then
+        verify(roomRepository).deleteById(fixedId);
+        verify(roomRepository).findById(fixedId);
+    }
 }
