4a288f992f255e7a2b4fc3d4676c072ec26977ce
Robert Stolz <robert.stolz87@gmail.com>
2024-12-07 12:44:54 +0100

    RoomServiceTest more tests added

--- backend/src/test/java/ropold/backend/service/RoomServiceTest.java
+++ backend/src/test/java/ropold/backend/service/RoomServiceTest.java
@@ -1,5 +1,6 @@
 package ropold.backend.service;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import ropold.backend.model.Category;
 import ropold.backend.model.RoomModel;
@@ -11,20 +12,88 @@ import java.util.Optional;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.*;
 
-
 class RoomServiceTest {
     IdService idService = mock(IdService.class);
     RoomRepository roomRepository = mock(RoomRepository.class);
     CloudinaryService cloudinaryService = mock(CloudinaryService.class);
-    RoomService testRoomService = new RoomService(idService, roomRepository,cloudinaryService);
+    RoomService testRoomService = new RoomService(idService, roomRepository, cloudinaryService);
 
-    RoomModel roomModel = new RoomModel("1", "Gürzenich Saal", "Martinstr. 29 50667 Köln",Category.ORCHESTER_HALL,"Ein traditionsreicher Saal für Konzerte und Veranstaltungen.",
-            "154427648", "Ropold", "https://avatars.githubusercontent.com/u/154427648?v=4",
-            "https://github.com/Ropold", true, "https://res.cloudinary.com/dzjjlydk3/image/upload/v1733477653/sqw62kggomno2bufjaoi.jpg");
-    RoomModel roomModel2 = new RoomModel("2","Bandraum *gemütlich*","Kreuzstraße 17, 50670 Köln",Category.BAND_ROOM,"Ein gemütlicher, gut gedämmter Raum zum Üben und Proben.",
-            "154427648","Ropold","https://avatars.githubusercontent.com/u/154427648?v=4","https://github.com/Ropold",false,"https://res.cloudinary.com/dzjjlydk3/image/upload/v1733473109/dauqufxqzou9akwyoxha.jpg");
+    RoomModel roomModel1 = new RoomModel(
+            "1", "Gürzenich Saal", "Martinstr. 29 50667 Köln",
+            Category.ORCHESTER_HALL, "Ein traditionsreicher Saal für Konzerte und Veranstaltungen.",
+            "123", "Testuser", "https://avatars-of-test-user.com/",
+            "https://github.com/Testuser", true, "https://res.cloudinary.com/Testuser/image/upload/Testuser/testimage1.jpg"
+    );
 
-    List<RoomModel> rooms = List.of(roomModel, roomModel2);
+    RoomModel roomModel2 = new RoomModel(
+            "2", "Beethoven-Saal", "Beethovenstraße 1, 53115 Bonn",
+            Category.ORCHESTER_HALL, "Ein moderner Saal für klassische Musik.",
+            "123", "Testuser", "https://avatars-of-test-user.com/",
+            "https://github.com/Testuser", false, "https://res.cloudinary.com/Testuser/image/upload/Testuser/testimage2.jpg"
+    );
+
+    List<RoomModel> rooms = List.of(roomModel1, roomModel2);
+
+    @BeforeEach
+    void setup() {
+        roomRepository.deleteAll();
+        roomRepository.saveAll(List.of(roomModel1, roomModel2));
+    }
+
+    @Test
+    void getActiveRooms() {
+        // Given
+        List<RoomModel> activeRooms = List.of(roomModel1);
+        when(roomRepository.findAll()).thenReturn(rooms);
+
+        // When
+        List<RoomModel> expected = testRoomService.getActiveRooms();
+
+        // Then
+        assertEquals(expected, activeRooms);
+    }
+
+    @Test
+    void toggleActiveStatus() {
+        // Given
+        RoomModel existingRoom = new RoomModel(
+                "1", "Gürzenich Saal", "Martinstr. 29 50667 Köln", Category.ORCHESTER_HALL,
+                "Ein traditionsreicher Saal für Konzerte und Veranstaltungen.", "123", "Testuser",
+                "https://avatars-of-test-user.com/", "https://github.com/Testuser", true,
+                "https://res.cloudinary.com/Testuser/image/upload/Testuser/testimage1.jpg"
+        );
+
+        RoomModel updatedRoom = new RoomModel(
+                "1", existingRoom.name(), existingRoom.address(), existingRoom.category(),
+                existingRoom.description(), existingRoom.appUserGithubId(), existingRoom.appUserUsername(),
+                existingRoom.appUserAvatarUrl(), existingRoom.appUserGithubUrl(), false,
+                existingRoom.imageUrl()
+        );
+
+        when(roomRepository.findById("1")).thenReturn(Optional.of(existingRoom));
+        when(roomRepository.save(any(RoomModel.class))).thenReturn(updatedRoom);
+
+        // When
+        RoomModel expected = testRoomService.toggleActiveStatus("1");
+
+        // Then
+        assertEquals(updatedRoom, expected);
+        verify(roomRepository).findById("1");
+        verify(roomRepository).save(updatedRoom);
+    }
+
+    @Test
+    void getRoomsByIds() {
+        // Given
+        List<String> roomIds = List.of("1", "2");
+        when(roomRepository.findAllById(roomIds)).thenReturn(rooms);
+
+        // When
+        List<RoomModel> expected = testRoomService.getRoomsByIds(roomIds);
+
+        // Then
+        assertEquals(expected, rooms);
+    }
 
     @Test
     void getAllRooms() {
@@ -37,26 +106,30 @@ class RoomServiceTest {
         // Then
         assertEquals(expected, rooms);
     }
+
     @Test
     void getRoomById() {
         // Given
-        when(roomRepository.findById("1")).thenReturn(Optional.of(roomModel));
+        when(roomRepository.findById("1")).thenReturn(Optional.of(roomModel1));
 
         // When
         RoomModel expected = testRoomService.getRoomById("1");
 
         // Then
-        assertEquals(expected, roomModel);
+        assertEquals(expected, roomModel1);
     }
 
     @Test
     void addRoom() {
         // Given
-        RoomModel roomModel3 = new RoomModel(null, "Gürzenich Saal", "Neumarkt 1, 50667 Köln", Category.ORCHESTER_HALL, "Ein traditionsreicher Saal für Konzerte und Veranstaltungen.",
-                "154427648", "Ropold", "https://avatars.githubusercontent.com/u/154427648?v=4", "https://github.com/Ropold", true, null);
+        RoomModel roomModel3 = new RoomModel(
+                null, "Großer Saal", "Neumarkt 1, 50667 Köln", Category.ORCHESTER_HALL,
+                "Ein traditionsreicher Saal für Konzerte und Veranstaltungen.", "123", "Testuser",
+                "https://avatars-of-test-user.com/", "https://github.com/Testuser", true, null
+        );
         RoomModel newRoom = new RoomModel("3", roomModel3.name(), roomModel3.address(), roomModel3.category(), roomModel3.description(),
-                roomModel3.appUserGithubId(), roomModel3.appUserUsername(), roomModel3.appUserAvatarUrl(), roomModel3.appUserGithubUrl(),
-                roomModel3.isActive(), "https://www.test.de/");
+                roomModel3.appUserGithubId(), roomModel3.appUserUsername(), roomModel3.appUserAvatarUrl(),
+                roomModel3.appUserGithubUrl(), roomModel3.isActive(), "https://res.cloudinary.com/Testuser/image/upload/Testuser/testimage3.jpg");
 
         when(idService.generateRandomId()).thenReturn("3");
         when(roomRepository.save(any(RoomModel.class))).thenReturn(newRoom);
@@ -72,27 +145,15 @@ class RoomServiceTest {
     void updateRoomWithPut() {
         // Given
         RoomModel existingRoom = new RoomModel(
-                "1",
-                "Gürzenich Saal",
-                "Neumarkt 1, 50667 Köln",
-                Category.ORCHESTER_HALL,
-                "Ein traditionsreicher Saal für Konzerte und Veranstaltungen.",
-                "154427648", "Ropold", "https://avatars.githubusercontent.com/u/154427648?v=4",
-                "https://github.com/Ropold", true, "https://www.test.de/"
+                "1", "Gürzenich Saal", "Neumarkt 1, 50667 Köln", Category.ORCHESTER_HALL,
+                "Ein traditionsreicher Saal für Konzerte und Veranstaltungen.", "123", "Testuser",
+                "https://avatars-of-test-user.com/", "https://github.com/Testuser", true, "https://www.test.de/"
         );
 
         RoomModel updatedRoom = new RoomModel(
-                "1",
-                existingRoom.name(),
-                existingRoom.address(),
-                existingRoom.category(),
-                existingRoom.description(),
-                existingRoom.appUserGithubId(),
-                existingRoom.appUserUsername(),
-                existingRoom.appUserAvatarUrl(),
-                existingRoom.appUserGithubUrl(),
-                false, // Updated isActive value
-                "https://www.test.de/"
+                "1", existingRoom.name(), existingRoom.address(), existingRoom.category(),
+                existingRoom.description(), existingRoom.appUserGithubId(), existingRoom.appUserUsername(),
+                existingRoom.appUserAvatarUrl(), existingRoom.appUserGithubUrl(), false, "https://www.test.de/"
         );
 
         when(roomRepository.existsById("1")).thenReturn(true);
@@ -108,10 +169,10 @@ class RoomServiceTest {
     }
 
     @Test
-    void deleteRoomTest() {
+    void deleteRoomWithImage() {
         // Given
         String fixedId = "1";
-        when(roomRepository.findById(fixedId)).thenReturn(Optional.of(roomModel));
+        when(roomRepository.findById(fixedId)).thenReturn(Optional.of(roomModel1));
 
         // When
         testRoomService.deleteRoom(fixedId);
@@ -119,5 +180,27 @@ class RoomServiceTest {
         // Then
         verify(roomRepository).deleteById(fixedId);
         verify(roomRepository).findById(fixedId);
+        verify(cloudinaryService).deleteImage(roomModel1.imageUrl());
+    }
+
+    @Test
+    void deleteRoomWithoutImage() {
+        // Given
+        RoomModel roomWithoutImage = new RoomModel(
+                "2", "Beethoven-Saal", "Beethovenstraße 1, 53115 Bonn", Category.ORCHESTER_HALL,
+                "Ein moderner Saal für klassische Musik.", "123", "Testuser",
+                "https://avatars-of-test-user.com/", "https://github.com/Testuser", false, null
+        );
+
+        String fixedId = "2";
+        when(roomRepository.findById(fixedId)).thenReturn(Optional.of(roomWithoutImage));
+
+        // When
+        testRoomService.deleteRoom(fixedId);
+
+        // Then
+        verify(roomRepository).deleteById(fixedId);
+        verify(roomRepository).findById(fixedId);
+        verify(cloudinaryService, never()).deleteImage(any());
     }
 }
