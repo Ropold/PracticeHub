371e311f6cab500434e612b3463baf6582e70502
Robert Stolz <robert.stolz87@gmail.com>
2024-12-09 14:45:52 +0100

    getAppUserFavorites changed to only ids

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -49,7 +49,9 @@ export default function App() {
     function getAppUserFavorites(){
         axios.get(`/api/practice-hub/favorites/${user}`)
             .then((response) => {
-                setFavorites(response.data)
+                const favoriteIds = response.data.map((favorite: any) => favorite.id);
+                setFavorites(favoriteIds);
+                //console.log("Extracted Favorite IDs:", favoriteIds);
             })
             .catch((error) => {
                 console.error(error);
