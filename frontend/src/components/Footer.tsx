252353eb3597bf9f300f54afb3a1fc86e42772ef
Robert Stolz <robert.stolz87@gmail.com>
2024-12-11 10:36:18 +0100

    pom & margin

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,11 +1,8 @@
-import "./styles/Footer.css"
 
 export default function Footer() {
     return (
-        <div>
             <footer>
                 <p>PracticeHub © 2024 by Ropold</p>
             </footer>
-        </div>
     )
 }
